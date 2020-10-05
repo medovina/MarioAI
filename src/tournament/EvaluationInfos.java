34e8d1362a7b0d924222ede07dbe75f1b0938af7
Adam Dingle <adam@medovina.org>
2020-10-05 17:09:18 +0200

    Report average score

--- src/tournament/EvaluationInfos.java
+++ src/tournament/EvaluationInfos.java
@@ -24,7 +24,10 @@ public class EvaluationInfos {
 	/**
 	 * In seconds; if the simulation runs headless, the time is approximated from the number of simulation frames.
 	 */
-	public double avgTimeSpent;
+    public double avgTimeSpent;
+    
+    public int totalScore;
+    public double avgScore;
 		
 	public List<EvaluationInfo> getResults() {
 		return results;
@@ -48,13 +51,15 @@ public class EvaluationInfos {
 		
 		results.add(result);
 		
-		totalTimeSpent += result.timeSpent;
+        totalTimeSpent += result.timeSpent;
+        totalScore += result.score;
 		
 		avgVictories = ((double)totalVictories) / ((double)results.size());
 		avgDeaths    = ((double)totalDeaths)    / ((double)results.size());
 		avgTimedout  = ((double)totalTimedout)  / ((double)results.size());
 		
-		avgTimeSpent = ((double)totalTimeSpent) / ((double)results.size());
+        avgTimeSpent = ((double)totalTimeSpent) / ((double)results.size());
+        avgScore = (double) totalScore / results.size();
 	}
 	
 	public void addResults(EvaluationInfo... results) {
@@ -74,7 +79,9 @@ public class EvaluationInfos {
 	}
 	
 	public String getCSVHeader() {
-		return "rounds;totalVictories;avgVictories;totalDeaths;avgDeaths;totalTimedout;avgTimedout;totalTimeSpent;avgTimeSpent";
+        return "rounds;totalVictories;avgVictories;totalDeaths;avgDeaths;" +
+               "totalTimedout;avgTimedout;totalTimeSpent;avgTimeSpent;" +
+               "totalScore;avgScore";
 	}
 	
 	public String getCSV() {
@@ -82,16 +89,17 @@ public class EvaluationInfos {
 			   + ";" + totalVictories + ";" + avgVictories
 			   + ";" + totalDeaths + ";" + avgDeaths
 			   + ";" + totalTimedout + ";" + avgTimedout
-			   + ";" + totalTimeSpent + ";" + avgTimeSpent;		
+			   + ";" + totalTimeSpent + ";" + avgTimeSpent
+			   + ";" + totalScore + ";" + avgScore;
 	}
 	
 	@Override
 	public String toString() {
         return String.format(
-            "won = %d (%.1f%%), died = %d (%.1f%%), timed out = %d (%.1f%%)",
+            "won = %d (%.1f%%), died = %d (%.1f%%), timed out = %d (%.1f%%), avg score = %.1f",
             totalVictories, 100 * avgVictories,
             totalDeaths, 100 * avgDeaths,
-            totalTimedout, 100 * avgTimedout);
+            totalTimedout, 100 * avgTimedout, avgScore);
 	}
 	
 }
